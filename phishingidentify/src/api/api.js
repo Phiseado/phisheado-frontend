f7cc1bea641d9affb72086805b3e366a9719d50c
Maribel Ramos <62081471+maribelrb22@users.noreply.github.com>
2022-06-09 12:23:57 +0200

    Adds statistics panel
    Co-Authored-By: Javier Vilariño Mayo <44314228+javivm17@users.noreply.github.com>

--- phishingidentify/src/api/api.js
+++ phishingidentify/src/api/api.js
@@ -6,6 +6,10 @@ async function apiPost(endpoint, body) {
     return await axios.post(`${backendUrl}${endpoint}`, body)
 }
 
+async function apiGet(endpoint) {
+    return await axios.get(`${backendUrl}${endpoint}`)
+}
+
 export async function analyseMessage(message) {
     let response = await apiPost('check/', message)
     return response.data
@@ -13,5 +17,10 @@ export async function analyseMessage(message) {
 
 export async function reportMessage(body) {
     let response = await apiPost('report-message/', body)
+    return response.data
+}
+
+export async function getDomains() {
+    let response = await apiGet('domains/')
     return response.data
 }
--- phishingidentify/src/components/Statistics.js
+++ phishingidentify/src/components/Statistics.js
@@ -1,19 +1,18 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Card } from 'primereact/card';
 import { Panel } from 'primereact/panel';
 import { DataScroller } from 'primereact/datascroller';
 import { Button } from 'primereact/button';
 import "../css/Statistics.css";
-import urls from "./urls.json";
+import { getDomains } from '../api/api';
 import { Splitter, SplitterPanel } from 'primereact/splitter';
 
 const UrlCard = (data) => {
     return (
         <Card className="p-card-urllist">
             <div className="flex flex-column">
-                <span><strong>Dominio:</strong> {data.domain}</span>
+                <span><strong>Dominio:</strong> {data.name}</span>
                 <span><strong>Frecuencia de aparición:</strong> {data.frequency}</span>
-                <span><strong>Países de aparición:</strong> {data.country}</span>
             </div>
         </Card>
     );
@@ -21,14 +20,21 @@ const UrlCard = (data) => {
 
 const Statistics = () => {
     const datascroller = useRef(null);
+    const [domains, setDomains] = useState([]);
     const footer = <Button className="p-button-custom mb-3" type="text" icon="pi pi-plus" label="Cargar" onClick={() => datascroller.current.load()} />
+
+    useEffect(() => {
+        getDomains().then(data => {
+            setDomains(data);
+        })
+    }, []);
 
     return (
         <Splitter className="p-c-splitter">
             <SplitterPanel className="flex align-items-center justify-content-center">
                 <Panel header="URL's consideradas como phishing" className="p-panel-urllist px-3 pt-3">
                     <div className="card">
-                        <DataScroller ref={datascroller} value={urls} itemTemplate={UrlCard} rows={3}
+                        <DataScroller ref={datascroller} value={domains} itemTemplate={UrlCard} rows={3}
                             loader footer={footer} />
                     </div>
                 </Panel>
